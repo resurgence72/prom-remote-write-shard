6d0b7243a6bf7108aeb737c33e498f4d3cb16b53
Resurgence72 <wuhaoyang1105@qq.com>
2023-11-10 16:05:09 +0800

    Update consistent_hash.go

--- pkg/consistent_hash.go
+++ pkg/consistent_hash.go
@@ -9,19 +9,39 @@ import (
 // - https://github.com/dgryski/go-rendezvous
 // - https://dgryski.medium.com/consistent-hashing-algorithmic-tradeoffs-ef6b8e2fcae8
 type ConsistentHash struct {
-	hashSeed   uint64
-	nodeHashes []uint64
+	hashSeed      uint64
+	replicaFactor int
+	nodeHashes    []uint64
 }
 
-func NewConsistentHash(nodes []string, hashSeed uint64) *ConsistentHash {
+func NewConsistentHash(nodes []string, replica int, hashSeed uint64) *ConsistentHash {
+	if replica <= 0 {
+		replica = 1
+	}
+
+	if replica > len(nodes) {
+		replica = len(nodes)
+	}
+
 	nodeHashes := make([]uint64, len(nodes))
 	for i, node := range nodes {
 		nodeHashes[i] = xxhash.Sum64([]byte(node))
 	}
 	return &ConsistentHash{
-		hashSeed:   hashSeed,
-		nodeHashes: nodeHashes,
+		hashSeed:      hashSeed,
+		nodeHashes:    nodeHashes,
+		replicaFactor: replica,
 	}
+}
+
+func (rh *ConsistentHash) GetReplicaNodeIdx(h uint64, excludeIdxs []int) []int {
+	var replicaNodeIdxs []int
+	for i := 0; i < rh.replicaFactor; i++ {
+		idx := rh.GetNodeIdx(h, excludeIdxs)
+		excludeIdxs = append(excludeIdxs, idx)
+		replicaNodeIdxs = append(replicaNodeIdxs, idx)
+	}
+	return replicaNodeIdxs
 }
 
 func (rh *ConsistentHash) GetNodeIdx(h uint64, excludeIdxs []int) int {
